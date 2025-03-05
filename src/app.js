1788585f5a76a42cf977ced7aec41edf85ffa6b4
Johan Mauricio <j.mauricior@icloud.com>
2025-03-05 08:51:49 +0000

    Generador de dominios actualizado con diseño Bootstrap y mejoras en el código

--- src/app.js
+++ src/app.js
@@ -1,11 +1,63 @@
+
 import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = function () {
+  console.log(" Generador de Nombres de Dominios cargado!");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  let pronombres = ["the", "our"];
+  let adjetivos = ["great", "big"];
+  let sustantivos = ["jogger", "racoon"];
+
+ 
+  let extensiones = [".com", ".net", ".us", ".io"];
+
+  
+  let hacks = {
+    "er": ".er",
+    "on": ".on",
+    "es": ".es",
+    "io": ".io"
+  };
+
+  
+  let resultadoDiv = document.getElementById("result");
+
+  
+  function generarDominios() {
+    resultadoDiv.innerHTML = ""; 
+
+    for (let p of pronombres) {
+      for (let a of adjetivos) {
+        for (let s of sustantivos) {
+          
+          
+          for (let ext of extensiones) {
+            agregarDominio(`${p}${a}${s}${ext}`);
+          }
+
+          
+          for (let hack in hacks) {
+            if (s.endsWith(hack)) {
+              let base = s.slice(0, -hack.length); 
+              agregarDominio(`${p}${a}${base}${hacks[hack]}`);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  
+  function agregarDominio(nombre) {
+    let p = document.createElement("p");
+    p.textContent = nombre;
+    p.classList.add("fw-bold", "text-success"); 
+    resultadoDiv.appendChild(p);
+  }
+
+  
+  generarDominios();
 };
